dd167cb6c1870d7fb76952fc3dccb16ca4c20bdf
Thiago Pecorari Clemente <thiagojapa2003@hotmail.com>
2020-03-15 23:48:39 -0300

    funcionalidade de editar

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,11 +12,12 @@ export default class Main extends Component {
   state = {
     novaTarefa: '',
     tarefas: [],
+    index: -1,
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
     novaTarefa = novaTarefa.trim();
 
@@ -24,9 +25,20 @@ export default class Main extends Component {
 
     const novasTarefas = [...tarefas];
 
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-    });
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+        novaTarefa: '',
+      });
+    }
   }
 
   handleChange = (e) => {
@@ -36,7 +48,11 @@ export default class Main extends Component {
   }
 
   handleEdit = (e, index) => {
-    console.log('Edit', index);
+    const { tarefas } = this.state;
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
   }
 
   handleDelete = (e, index) => {
